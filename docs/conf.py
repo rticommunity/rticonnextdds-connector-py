36da4fa4db49d939bcef3e0337a148d9089f9a1d
Alex Campos <alejandro@rti.com>
2019-07-11 13:55:01 -0700

    CON-115: Support older sphinx versions

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ author = 'RTI'
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
