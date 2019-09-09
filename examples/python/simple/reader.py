974ae3b035a13909ee8fe6c1cb588f19fedf4460
samuelraeburn <raeburnsamuel@gmail.com>
2019-09-09 16:09:50 +0200

    CON-134: Re-wrote the simple reader/writer example

--- examples/python/simple/reader.py
+++ examples/python/simple/reader.py
@@ -1,39 +1,52 @@
 ###############################################################################
-# (c) 2005-2015 Copyright, Real-Time Innovations.  All rights reserved.       #
+# (c) 2005-2019 Copyright, Real-Time Innovations.  All rights reserved.       #
 # No duplications, whole or partial, manual or electronic, may be made        #
 # without express written permission.  Any such copies, or revisions thereof, #
 # must display this notice unaltered.                                         #
 # This code contains trade secrets of Real-Time Innovations, Inc.             #
 ###############################################################################
 
-"""Samples's reader."""
-
 from __future__ import print_function
 from sys import path as sysPath
 from os import path as osPath
-from time import sleep
-
 filepath = osPath.dirname(osPath.realpath(__file__))
 sysPath.append(filepath + "/../../../")
 import rticonnextdds_connector as rti
 
+with rti.open_connector("MyParticipantLibrary::MyInputParticipant", filepath + "/../ShapeExample.xml") as connector:
 
-with rti.open_connector("MyParticipantLibrary::MyParticipant", filepath + "/../ShapeExample.xml") as connector:
+    # Waits until the_input matches with a publication named publication_name
+    def wait_for_discovery(the_input, publication_name):
+        print("Waiting to match with " + publication_name)
+        matches = []
+        while {'name': publication_name} not in matches:
+            # wait_for_publications returns the change in matches since it was last called
+            changes_in_matches = the_input.wait_for_publications(2000)
+            if changes_in_matches > 0:
+                # get_matched_publications returns a list of all matched writers
+                matches = the_input.get_matched_publications()
+        for match in matches:
+            print("Matched with: %s" % match["name"])
 
+    # Obtain the input which we use to receive data
     dds_input = connector.get_input("MySubscriber::MySquareReader")
 
+    # Wait for the input and output to discover each other
+    wait_for_discovery(dds_input, "MySquareWriter")
+
     for i in range(1, 500):
+        # Wait for data to be available on the input
+        dds_input.wait(2000)
+        # Take the data, removing it from the queue
         dds_input.take()
         for sample in dds_input.valid_data_iterator:
-                # You can get all the fields in a get_dictionary()
-                data = sample.get_dictionary()
-                x = data['x']
-                y = data['y']
-
-                # Or you can access the field individually
-                size = sample.get_number("shapesize")
-                color = sample.get_string("color")
-                print("Received x: " + repr(x) + " y: " + repr(y) + \
-                        " size: " + repr(size) + " color: " + repr(color))
-
-        sleep(2)
+            # There are a variety of methods available for obtaining the data:
+            # 1. Index the sample using the field name
+            x = sample['x']
+            # 2. Obtain a dictionary of the sample using get_dictionary()
+            the_dict = sample.get_dictionary()
+            y = the_dict['y']
+            color = the_dict['color']
+            # 3. Obtain the value directly use the get_X APIs
+            size = sample.get_number("shapesize")
+            print("Received x: %d, y: %d, size: %d, color: %s" % (x, y, size, color))
--- examples/python/simple/writer.py
+++ examples/python/simple/writer.py
@@ -1,28 +1,47 @@
 ###############################################################################
-# (c) 2005-2015 Copyright, Real-Time Innovations.  All rights reserved.       #
+# (c) 2005-2019 Copyright, Real-Time Innovations.  All rights reserved.       #
 # No duplications, whole or partial, manual or electronic, may be made        #
 # without express written permission.  Any such copies, or revisions thereof, #
 # must display this notice unaltered.                                         #
 # This code contains trade secrets of Real-Time Innovations, Inc.             #
 ###############################################################################
 
-"""Samples's writer."""
-
 from sys import path as sysPath
 from os import path as osPath
-from time import sleep
 filepath = osPath.dirname(osPath.realpath(__file__))
 sysPath.append(filepath + "/../../../")
 import rticonnextdds_connector as rti
 
-connector = rti.Connector("MyParticipantLibrary::MyParticipant",
-                          filepath + "/../ShapeExample.xml")
-outputDDS = connector.get_output("MyPublisher::MySquareWriter")
+with rti.open_connector("MyParticipantLibrary::MyOutputParticipant", filepath + "/../ShapeExample.xml") as connector:
 
-for i in range(1, 500):
-    outputDDS.instance.set_number("x", i)
-    outputDDS.instance.set_number("y", i*2)
-    outputDDS.instance.set_number("shapesize", 30)
-    outputDDS.instance.set_string("color", "BLUE")
-    outputDDS.write()
-    sleep(2)
+    # Waits until the_output matches with a subscription named subscription_name
+    def wait_for_discovery(the_output, subscription_name):
+        print("Waiting to match with " + subscription_name)
+        matches = []
+        while {'name': subscription_name} not in matches:
+            # wait_for_publications returns the change in matches since it was last called
+            changes_in_matches = the_output.wait_for_subscriptions(2000)
+            if changes_in_matches > 0:
+                # get_matched_publications returns a list of all matched writers
+                matches = the_output.get_matched_subscriptions()
+        for match in matches:
+            print("Matched with: %s" % match["name"])
+
+    dds_output = connector.get_output("MyPublisher::MySquareWriter")
+
+    # Wait for discovery to occur
+    wait_for_discovery(dds_output, "MySquareReader")
+
+    for i in range(1, 500):
+        print("Writing sample %d" % i)
+        # We can set the data into the sample in a variety of ways:
+        # 1. Indexing it directly:
+        dds_output.instance['x'] = i
+        dds_output.instance['shapesize'] = 30
+        # 2. Using the set_X APIs:
+        dds_output.instance.set_number("y", i*2)
+        dds_output.instance.set_string("color", "BLUE")
+        # Write the data using the write() API
+        dds_output.write()
+        # Wait for all the matched subscriptions to acknowledge the sample
+        dds_output.wait(2000)
