9a190cb2ac666ea7aa3cf5bc7c18bd2879dc260b
samuelraeburn <raeburnsamuel@gmail.com>
2019-09-10 13:20:36 +0200

    CON-134: Modified read_and_write example

--- examples/python/simple/read_and_write.py
+++ examples/python/simple/read_and_write.py
@@ -6,43 +6,61 @@
 # This code contains trade secrets of Real-Time Innovations, Inc.             #
 ###############################################################################
 
-"""Read a sample and write a new one."""
+"""Read a sample and write a new one.
+   This example is designed to be used with rtishapesdemo.
+   Launch an instance of shapes demo and publish a square (any color other
+   than purple) and subscribe to squares.
+   This example will read the samples published by the instance of rtishapesdemo,
+   modify the data and re-publish them."""
 
 from sys import path as sysPath
 from os import path as osPath
+from time import sleep
 filepath = osPath.dirname(osPath.realpath(__file__))
 sysPath.append(filepath + "/../../../")
 import rticonnextdds_connector as rti
 
 with rti.open_connector("MyParticipantLibrary::MyParticipant", filepath + "/../ShapeExample.xml") as connector:
 
-    dds_input = connector.get_input("MySubscriber::MySquareReader")
     dds_output = connector.get_output("MyPublisher::MySquareWriter")
+    dds_input = connector.get_input("MySubscriber::MySquareReader")
 
     # This example reads a sample received from a remote publication, modifies
     # the data in it and writes it back out. We need to wait until we discover
-    # the remote writer. We will internally match with the writer which we
-    # have created above, but need an additional match.
-    # total_matches = 0
-    # while total_matches < 2:
-    #     # wait_for_publications can return a negative value if we unmatch
-    #     new_matches = dds_input.wait_for_publications(2000)
-    #     if new_matches > 0:
-    #         total_matches += new_matches
+    # the remote writer and reader. We do not explicitly check publication_name or
+    # subscription_name of the matched entity to allow this example to work
+    # with other DDS applications (e.g., rtishapesdemo)
+    total_matches = 0
+    while total_matches < 2:
+        # wait_for_publications can return a negative value if we unmatch
+        new_matches = dds_input.wait_for_publications(2000)
+        if new_matches > 0:
+            total_matches += new_matches
+    for match in dds_input.get_matched_publications():
+        print("Matched publications: %s" % match["name"])
+    total_matches = 0
+    while total_matches < 2:
+        # wait_for_publications can return a negative value if we unmatch
+        new_matches = dds_output.wait_for_subscriptions(2000)
+        if new_matches > 0:
+            total_matches += new_matches
+    for match in dds_output.get_matched_subscriptions():
+        print("Matched subscriptions: %s" % match["name"])
 
-    for i in range(1, 5000):
+    for i in range(1, 500):
         # Wait for data to arrive
         dds_input.wait(2000)
         # Take the data
         dds_input.take()
         for sample in dds_input.valid_data_iterator:
-            # Obtain the dictionary of the sample
-            dictionary = sample.get_dictionary()
-            # Modify the data (x <-> y, color -> RED)
-            # dictionary['x'], dictionary['y'] = dictionary['y'], dictionary['x']
-            dictionary['color'] = "PURPLE" 
-            dictionary['shapesize'] = dictionary['shapesize'] / 2
-            dds_output.instance.set_dictionary(dictionary)
-            dds_output.write()
-            # Wait for all matched readers to acknowledge the sample
-            dds_output.wait(2000)
+            # We don't want to take our own samples
+            if sample['color'] != "PURPLE":
+                dictionary = sample.get_dictionary()
+                # Modify the data (x <-> y, color -> purple)
+                dictionary['x'], dictionary['y'] = dictionary['y'], dictionary['x']
+                dictionary['color'] = "PURPLE" 
+                dds_output.instance.set_dictionary(dictionary)
+                dds_output.write(writer)
+
+    # Wait for all matched readers to acknowledge the sample
+    dds_output.wait(2000)
