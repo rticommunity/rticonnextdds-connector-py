48fc1da785ba218c6023c4808a8739ec21c201bd
samuelraeburn <raeburnsamuel@gmail.com>
2020-07-13 10:22:58 +0200

    Separated tests

--- test/python/test_rticonnextdds_performance.py
+++ test/python/test_rticonnextdds_performance.py
@@ -26,14 +26,10 @@ class TestPerformance:
     def num_iterations(self):
         return 100
 
-    # Test how long it takes to set a sequence of 600000 elements.
-    # There are 2 options available:
-    # for i in range(0, 600000):
-    #   output.instance['myOctSeq[i]'] = 1
-    # or
-    # output.instance['myOctSeq'] = [1, 2, 3, 4, 5, ....]
-    # Test both approaches (expected to be slow until we implement CON-42)
-    def test_set_sequence(self, one_use_connector, num_iterations):
+    # The use-case of setting a sequence in Connector is slow, and likely will
+    # be until we implement CON-42.
+    # Here we time how long it takes to set a sequence element by element.
+    def test_set_sequence_element_by_element(self, one_use_connector, num_iterations):
         # Get the input and output which communicate using the performance test type
         the_input = one_use_connector.get_input("MySubscriber::PerformanceTestReader")
         the_output = one_use_connector.get_output("MyPublisher::PerformanceTestWriter")
@@ -42,7 +38,6 @@ class TestPerformance:
         the_input.wait_for_publications(5000)
         the_output.wait_for_subscriptions(5000)
 
-        # Approach 1.
         # Set each element of the sequence separately
         total_time = 0
         for i in range(0, num_iterations):
@@ -54,14 +49,26 @@ class TestPerformance:
         print("Average time setting element-by-element: " + str(average_time))
         # Average time setting element-by-element: 2.9646800351142883
 
+    # The use-case of setting a sequence in Connector is slow, and likely will
+    # be until we implement CON-42.
+    # Here we time how long it takes to set a sequence from a Python list.
+    # i.e., output.instance['myOctSeq'] = [1, 2, 3, 4, 5, ....]
+    def test_set_sequence_from_list(self, one_use_connector, num_iterations):
+        # Get the input and output which communicate using the performance test type
+        the_input = one_use_connector.get_input("MySubscriber::PerformanceTestReader")
+        the_output = one_use_connector.get_output("MyPublisher::PerformanceTestWriter")
 
-        # Approach 2.
-        # Create a python list which contains 600000 and set it in one call
+        # Wait for discovery between the entities
+        the_input.wait_for_publications(5000)
+        the_output.wait_for_subscriptions(5000)
+
+        # Create a python list which contains 600000 and set the sequence from it
         total_time = 0
         average_time = 0
         myOctSeq = [0]
         for i in range (1, 600000):
             myOctSeq.append(i)
+
         for i in range (0, num_iterations):
             start_time = time.time()
             the_output.instance['myOctSeq'] = myOctSeq
@@ -70,10 +77,10 @@ class TestPerformance:
         print("Average time setting entire list in one go: " + str(average_time))
         # Note to self Average time: 6.60276771068573
 
-    # The use-case of obtainine-case of obtaining a dictionary containing a sequence in Connector
-    # is slow, until we implement CON-42
-    # Here we have a sequenc with 600000 elements. We time how long it takes to
-    # access
+    # The use-case of obtaining a dictionary containing a sequence in Connector
+    # is slow, and likely will be until we implement CON-42.
+    # Here we have a sequence with 600000 elements. We time how long it takes to
+    # obtain this sequence as a Python list
     def test_get_sequence(self, one_use_connector, num_iterations):
         # Get the input and output which communicate using the performance test type
         the_input = one_use_connector.get_input("MySubscriber::PerformanceTestReader")
