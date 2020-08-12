392c15b690193695c280472bec3bc461cf98e21e
Alex Campos <alejandro@rti.com>
2020-08-12 14:00:50 -0700

    CON-209: Test support for multiple urls in connector creation

--- test/python/test_rticonnextdds_connector.py
+++ test/python/test_rticonnextdds_connector.py
@@ -6,71 +6,88 @@
 # This code contains trade secrets of Real-Time Innovations, Inc.             #
 ###############################################################################
 
-import sys,time,os,pytest
-sys.path.append(os.path.dirname(os.path.realpath(__file__))+ "/../../")
+import sys
+import os
 import rticonnextdds_connector as rti
+import pytest
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../")
+
 
 class TestConnector:
-  """
-  This class tests the correct instantiation of
-  :class:`rticonnextdds_connector.Connector` object.
-  """
+    """
+    This class tests the correct instantiation of
+    :class:`rticonnextdds_connector.Connector` object.
+    """
 
-  def test_invalid_xml_path(self):
-    """
-    This test function ensures that a ValueError is raised if
-    an incorrect xml path is passed to the
-    Connector constructor.
-    """
-    participant_profile = "MyParticipantLibrary::Zero"
-    invalid_xml_path = "invalid/path/to/xml"
-    with pytest.raises(rti.Error):
-      connector = rti.Connector(participant_profile,invalid_xml_path)
+    def test_invalid_xml_path(self):
+        """
+        This test function ensures that a ValueError is raised if
+        an incorrect xml path is passed to the
+        Connector constructor.
+        """
+        participant_profile = "MyParticipantLibrary::Zero"
+        invalid_xml_path = "invalid/path/to/xml"
+        with pytest.raises(rti.Error):
+            rti.Connector(participant_profile, invalid_xml_path)
 
-  def test_invalid_participant_profile(self):
-    """
-    This test function ensures that a ValueError is raised if
-    an invalid participant profile name is passed to the
-    Connector constructor.
-    """
-    invalid_participant_profile = "InvalidParticipantProfile"
-    xml_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-      "../xml/TestConnector.xml")
-    with pytest.raises(rti.Error):
-      connector = rti.Connector(invalid_participant_profile,xml_path)
+    def test_invalid_participant_profile(self):
+        """
+        This test function ensures that a ValueError is raised if
+        an invalid participant profile name is passed to the
+        Connector constructor.
+        """
+        invalid_participant_profile = "InvalidParticipantProfile"
+        xml_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                "../xml/TestConnector.xml")
+        with pytest.raises(rti.Error):
+            rti.Connector(invalid_participant_profile, xml_path)
 
-  def test_ivalid_xml_profile(self):
-    """
-    This test function ensures that a ValueError is raised if
-    an invalid xml file is passed to the
-    Connector constructor.
-    """
-    participant_profile = "MyParticipantLibrary::Zero"
-    invalid_xml = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-      "../xml/InvalidXml.xml")
-    with pytest.raises(rti.Error):
-      connector = rti.Connector(participant_profile,invalid_xml)
+    def test_ivalid_xml_profile(self):
+        """
+        This test function ensures that a ValueError is raised if
+        an invalid xml file is passed to the
+        Connector constructor.
+        """
+        participant_profile = "MyParticipantLibrary::Zero"
+        invalid_xml = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                   "../xml/InvalidXml.xml")
+        with pytest.raises(rti.Error):
+            rti.Connector(participant_profile, invalid_xml)
 
-  def test_connector_creation(self,rtiConnectorFixture):
-    """
-    This function tests the correct instantiation of
-    Connector object.
+    def test_connector_creation(self, rtiConnectorFixture):
+        """
+        This function tests the correct instantiation of
+        Connector object.
+        """
+        assert rtiConnectorFixture is not None and isinstance(
+            rtiConnectorFixture, rti.Connector)
 
-    :param rtiConnectorFixture: :func:`conftest.rtiConnectorFixture`
-    :type rtiConnectorFixture: `pytest.fixture <https://pytest.org/latest/builtin.html#_pytest.python.fixture>`_
-    """
-    assert rtiConnectorFixture!=None and isinstance(rtiConnectorFixture,\
-      rti.Connector)
+    def test_multiple_connector_creation(self):
+        """
+        This function tests the correct instantiation of multiple
+        Connector objects in succession.
+        """
+        connectors = []
+        xml_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                "../xml/TestConnector.xml")
+        participant_profile = "MyParticipantLibrary::Zero"
+        for _ in range(0, 5):
+            connectors.append(rti.Connector(participant_profile, xml_path))
+        assert all(x is not None and isinstance(x, rti.Connector)
+                   for x in connectors)
 
-  def test_multiple_connector_creation(self):
-    """
-    This function tests the correct instantiation of multiple
-    Connector objects in succession.
-    """
-    connectors = []
-    xml_path= os.path.join(os.path.dirname(os.path.realpath(__file__)),
-      "../xml/TestConnector.xml")
-    participant_profile="MyParticipantLibrary::Zero"
-    for i in range (0,5):
-     connectors.append(rti.Connector(participant_profile,xml_path))
-    assert all( x!=None and isinstance(x,rti.Connector) for x in connectors)
+    def test_load_multiple_files(self):
+        """
+        Tests that it is possible to load two xml files using the url group syntax
+        """
+        xml_path1 = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                 "../xml/TestConnector.xml")
+        xml_path2 = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                 "../xml/TestConnector2.xml")
+        with rti.open_connector(
+                config_name="MyParticipantLibrary2::MyParticipant2",
+                url=xml_path1 + ';' + xml_path2) as connector:
+
+            assert connector is not None
+            output = connector.get_output("MyPublisher2::MySquareWriter2")
+            assert output is not None
