5c72d0d8572327ccf518a531aedb767e71eff844
Bob Garrow <bgarrowjr@gmail.com>
2021-11-18 10:57:01 -0800

    HMAINT-202 -- correct rc1 version string (#103)

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,8 @@ copyright = '2021, Real-Time Innovations, Inc'
 author = 'Real-Time Innovations, Inc.'
 
 # The full version, including alpha/beta/rc tags
-version = '1.2.0.rc1'
-release = '1.2.0.rc1'
+version = '1.2.0-rc1'
+release = '1.2.0-rc1'
 
 master_doc = 'index'
 
