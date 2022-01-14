e955e4b63fd8a29de7bdfbb3f2613d1f9cba8a3a
Bob Garrow <bgarrowjr@gmail.com>
2022-01-14 08:20:16 -0800

    #fix SUPPORT-298 -- change version to 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,8 @@ copyright = '2019, Real-Time Innovations, Inc'
 author = 'Real-Time Innovations, Inc.'
 
 # The full version, including alpha/beta/rc tags
-version = '1.0.1-rc1'
-release = '1.0.1-rc1'
+version = '1.0.1'
+release = '1.0.1'
 
 master_doc = 'index'
 
