4eaaeb35b1c3fcaf6e79dd29ce41ac6848a22f4c
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2024-09-04 09:03:18 +0200

    COMMUNITY-100: use python 3.8 container

--- resources/jenkins/build_doc.groovy
+++ resources/jenkins/build_doc.groovy
@@ -12,9 +12,8 @@
 
 pipeline {
     agent {
-        dockerfile {
-            additionalBuildArgs  "--build-arg USER_UID=789"
-            dir 'resources/docker'
+        docker {
+            image 'python:3.8'
             label 'docker'
         }
     }
@@ -45,7 +44,6 @@ pipeline {
         stage('Build doc') {
             steps {
                 dir('docs') {
-                    sh 'pip install --upgrade pip'
                     sh 'pip install -r requirements.txt --no-cache-dir'
                     sh 'make docs html'
                 }
